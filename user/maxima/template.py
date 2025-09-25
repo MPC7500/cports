12e72247012ca62e5a545bc57a43d911cc2f5d1a
MPC7500 <meldungan+github@posteo.de>
2025-09-25 17:57:00 +0200

    new package maxima

--- user/maxima/template.py
+++ user/maxima/template.py
@@ -7,6 +7,7 @@ configure_args = [
     "--with-gmp",
     "--prefix=/usr",
     "--infodir=/usr/share/info"
+    "--mandir=/usr/share/man",
 ]
 hostmakedepends = [
     "autoconf",
@@ -22,6 +23,9 @@ depends = [
     "gnuplot",
     "sbcl",
 ]
+checkdepends = [
+    "perl",
+]
 pkgdesc = "Computer algebra system based on Lisp"
 license = "GPL-2.0-or-later"
 url = "http://maxima.sourceforge.net"
@@ -29,6 +33,12 @@ source = f"$(SOURCEFORGE_SITE)/{pkgname}/{pkgname}-{pkgver}.tar.gz"
 sha256 = "b0916b5fb37b6eeaae400083175e68e28f80b9a1ab580c106a05448cf1c496b2"
 # take hours
 options = ["!check"]
+
+def post_install(self):
+    self.install_file("doc/info/maxima-index.lisp",
+        "usr/share/info", name="maxima-index.lisp")
+    self.install_file("doc/info/maxima-index-html.lisp",
+        "usr/share/info", name="maxima-index-html.lisp")
 
 def check(self):
     self.make.check()
