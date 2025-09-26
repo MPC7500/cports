9a2110b0e2871bbc2cff5fb2f4188790cc58c7b6
MPC7500 <meldungan+github@posteo.de>
2025-09-26 15:54:19 +0200

    new package: WxMaxima

---
+++ user/wxmaxima/template.py
@@ -0,0 +1,37 @@
+pkgname = "wxmaxima"
+pkgver = "25.04.0"
+pkgrel = 0
+build_style = "cmake"
+configure_args = [
+    f"-DCMAKE_INSTALL_PREFIX=/usr",
+    f"-DCMAKE_INSTALL_DATADIR=/usr/share",
+]
+hostmakedepends = [
+    "cmake",
+    "ninja",
+    "pkgconf",
+]
+makedepends = [
+	"gettext-devel",
+    "wxwidgets-devel",
+]
+depends = ["maxima"]
+pkgdesc = "Graphische Oberfläche für Maxima basierend auf wxWidgets"
+license = "GPL-2.0-or-later"
+url = "https://wxmaxima-developers.github.io/wxmaxima"
+source = f"https://github.com/wxMaxima-developers/wxmaxima/archive/refs/tags/Version-{pkgver}.tar.gz"
+sha256 = "ec0b3005c3663f1bb86b0cc5028c2ba121e1563e3d5b671afcb9774895f4191b"
+# test
+options = ["!check"]
+
+def post_install(self):
+    self.install_license("COPYING")
+    
+    import os
+    pixmaps = os.path.join(self.destdir, "usr/share/pixmaps")
+    if os.path.isdir(pixmaps):
+        try:
+            os.rmdir(pixmaps)
+            print("Removed empty /usr/share/pixmaps to satisfy lint.")
+        except OSError:
+            print("/usr/share/pixmaps exists and is not empty; leaving it in place.")
