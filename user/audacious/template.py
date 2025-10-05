512ea406398d5f9e7cfb0ff3e5dcd3e7bfc7709e
MPC7500 <meldungan+github@posteo.de>
2025-10-05 19:25:59 +0200

    new package: audacious final

--- user/audacious/template.py
+++ user/audacious/template.py
@@ -13,14 +13,18 @@ makedepends = [
     "glib-devel",
     "gtk+3-devel",
     "libxml2-devel",
-    "qt5-qtbase-devel",
-    "qt5-qtsvg-devel",
+    "qt6-qtbase-devel",
+    "qt6-qtsvg-devel",
 ]
 pkgdesc = "Lightweight and versatile audio player"
 license = "BSD-2-Clause"
 url = "https://audacious-media-player.org"
 source = f"https://distfiles.audacious-media-player.org/audacious-{pkgver}.tar.bz2"
-sha256 = "SKIP"
+sha256 = "7194743a0a41b1d8f582c071488b77f7b917be47ca5e142dd76af5d81d36f9cd"
 
 def post_install(self):
     self.install_license("COPYING")
+
+@subpackage("audacious-devel")
+def _(self):
+    return self.default_devel()
